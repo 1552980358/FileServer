a94ff591122f1c8d11ecd4624bfa285819c854c8
1552980358 <1552980358@qq.com>
2020-06-23 08:52:14 +0800

    Modify function `login`

--- src/main/kotlin/Authorization.kt
+++ src/main/kotlin/Authorization.kt
@@ -1,4 +1,5 @@
 import lib.github1552980358.ktExtension.jvm.io.writeAndClose
+import lib.github1552980358.ktExtension.jvm.keyword.tryCatch
 import java.io.BufferedOutputStream
 import java.io.File
 import java.math.BigInteger
@@ -27,10 +28,13 @@ class Authorization: HttpServlet() {
         
         /** Respond message **/
         private const val RESPOND_HEAD = "respond"
+        
         /** Unknown found **/
         private const val RESPOND_UNKNOWN_TYPE = "unknown_type"
         private const val RESPOND_UNKNOWN_ID_PW = "unknown_id_pw"
+        private const val RESPOND_ID_PW_NOT_SPECIFIED = "id_pw_not_specified"
         private const val RESPOND_UNKNOWN_TOKEN = "unknown_token"
+        
         /** Success **/
         private const val RESPOND_LOGIN_SUCCESS = "login_success"
         private const val RESPOND_LOGIN_SUCCESS_TOKEN = "token"
@@ -55,17 +59,9 @@ class Authorization: HttpServlet() {
             REQUEST_TYPE_AUTHORIZE -> {
         
             }
-            REQUEST_TYPE_LOGIN -> {
-                login(req, resp)
-            }
-            else -> {
-                try {
-                    unknown(resp)
-                    return
-                } catch (e: Exception) {
-                    e.printStackTrace()
-                }
-            }
+            REQUEST_TYPE_LOGIN -> login(req, resp)
+            
+            else -> tryCatch { unknown(resp) }
         }
         
     }
@@ -74,17 +70,35 @@ class Authorization: HttpServlet() {
         val ac = req.getParameter(LOGIN_AC)
         val pw = req.getParameter(LOGIN_PW)
         
+        if (ac == null || pw == null) {
+            tryCatch { resp.outputStream.writeAndClose("{\"$RESPOND_HEAD\"=\"$RESPOND_ID_PW_NOT_SPECIFIED\"}") }
+            return
+        }
+        
         resp.contentType = "application/json"
         val dir = File(FILE_CONFIG)
         dir.readLines().apply {
+            if (size < 2) {
+                return
+            }
             if (!checkAcPw(ac, this[0], resp) || !checkAcPw(pw, this[1], resp)) {
                 return
             }
         }
         
         val sha256 = getSHA256(dir.readBytes())
-        resp.outputStream.writeAndClose("{\"$RESPOND_HEAD\"=\"$RESPOND_LOGIN_SUCCESS\",\"$RESPOND_LOGIN_SUCCESS_TOKEN\"$sha256}")
-        File(FILE_SHA256).writeText(sha256)
+        tryCatch {
+            resp.outputStream.writeAndClose(
+                "{\"$RESPOND_HEAD\"=\"$RESPOND_LOGIN_SUCCESS\",\"$RESPOND_LOGIN_SUCCESS_TOKEN\"$sha256}"
+            )
+        }
+        File(FILE_SHA256).apply {
+            if (exists()) {
+                tryCatch { delete() }
+            }
+            tryCatch { createNewFile() }
+            tryCatch { writeText(sha256) }
+        }
     }
     
     private fun authorize(req: HttpServletRequest, resp: HttpServletResponse) {
@@ -122,7 +136,11 @@ class Authorization: HttpServlet() {
             return result
         }
         
-        return StringBuilder().apply { for (i in 0 until 32 - result.length) { append('0') } }.toString() + result
+        return StringBuilder().apply {
+            for (i in 0 until 32 - result.length) {
+                append('0')
+            }
+        }.toString() + result
     }
     
 }
