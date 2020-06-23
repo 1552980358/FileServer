e0095243d874921a43082b118ca8df0e62201ec9
1552980358 <1552980358@qq.com>
2020-06-23 09:58:00 +0800

    Update Upload.kt

---
+++ src/main/kotlin/Upload.kt
@@ -0,0 +1,17 @@
+import javax.servlet.annotation.WebServlet
+import javax.servlet.http.HttpServlet
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+@WebServlet("/upload")
+class Upload: HttpServlet() {
+    
+    override fun doGet(req: HttpServletRequest?, resp: HttpServletResponse?) {
+    
+    }
+    
+    override fun doPost(req: HttpServletRequest?, resp: HttpServletResponse?) {
+    
+    }
+    
+}
